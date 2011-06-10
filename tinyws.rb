49b7d419d17876f4fd55f505290e653f7016f34e
Larry Glenn <yrral.glenn@gmail.com>
2011-06-10 19:35:57 -0400

    tinyws now takes port as the value of the -p/--port switch, and docroot as ARGV[0]
    Also added signal handling, so the server shuts down on sigint / sigterm.

--- tinyws.rb
+++ tinyws.rb
@@ -1,8 +1,31 @@
+#!/usr/bin/ruby
+
+require 'optparse'
 require 'webrick'
 include WEBrick
 
-root = '/Users/larry/Projects/TinyWS/docroot'
 port = 8080
-config = {  :Port => port, :DocumentRoot => root }
-server = WEBrick::HTTPServer.new(config)
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: tinyws [options] <docroot>"
+  opts.on('-p', '--port PORT', 'Listen port' ) do |p|
+    port = p
+  end
+end.parse!
+
+if ARGV.length != 1
+  raise OptionParser::MissingArgument
+end
+
+docroot = ARGV[0]
+
+server = WEBrick::HTTPServer.new(:Port => port, :DocumentRoot => docroot)
+
+yield server if block_given?
+
+# Shut down on sigint, sigterm
+[ 'INT', 'TERM' ].each {  |signal|
+  trap(signal) { server.shutdown }
+}
+
 server.start
